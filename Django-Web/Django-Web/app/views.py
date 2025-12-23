f99e308c7f814f697d34a56afbdf772e14ee9340
SaadRiaz99 <140387256+SaadRiaz99@users.noreply.github.com>
2025-12-24 00:08:47 +0500

    Delete Django-Web directory

--- Django-Web/Django-Web/app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request, 'Index.html')
-def Services(request):
-    return render(request, 'Services.html')
-def Feature(request):
-    return render(request, 'Feature.html')
